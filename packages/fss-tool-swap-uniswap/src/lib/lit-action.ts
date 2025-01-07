921acb564d4edc2d75e153c8f21d08a577303514
awisniew207 <awisniew@uoregon.edu>
2025-01-06 20:08:00 -0800

    uniswap ethereum/arb support

--- packages/fss-tool-swap-uniswap/src/lib/lit-action.ts
+++ packages/fss-tool-swap-uniswap/src/lib/lit-action.ts
@@ -31,7 +31,7 @@ declare global {
   };
   const params: {
     rpcUrl: string;
-    chainId: number;
+    chainId: string;
     tokenIn: string;
     tokenOut: string;
     amountIn: string;
@@ -42,8 +42,21 @@ type JsonRpcProvider = ReturnType<typeof ethers.providers.JsonRpcProvider>;
 
 export default async () => {
   try {
-    const UNISWAP_V3_QUOTER = "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a";
-    const UNISWAP_V3_ROUTER = "0x2626664c2603336E57B271c5C0b26F421741e481";
+    let UNISWAP_V3_QUOTER: string;
+    let UNISWAP_V3_ROUTER: string;
+    switch(params.chainId) {
+      case "8453":
+        UNISWAP_V3_QUOTER = "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a";
+        UNISWAP_V3_ROUTER = "0x2626664c2603336E57B271c5C0b26F421741e481";
+        break;
+      case "1":
+      case "42161":
+        UNISWAP_V3_QUOTER = "0x61fFE014bA17989E743c5F6cB21bF9697530B21e";
+        UNISWAP_V3_ROUTER = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
+        break;
+      default:
+        throw new Error(`Unsupported chain ID: ${params.chainId}`);
+    }
 
     // Check if the session signer is a delegatee
     async function checkLitAuthAddressIsDelegatee(
